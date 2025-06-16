ac8bbe5419a9cfe10cf78de6853c9bc88420c7ad
ErVaaL <ErVaLofficial@protonmail.com>
2025-06-16 22:18:42 +0200

    Removed unused

--- entry_cli.go
+++ entry_cli.go
@@ -8,17 +8,6 @@ import (
 	"os"
 )
 
-func runCombinedMain() {
-	args := os.Args[1:]
-
-	if len(args) == 0 || args[0] == "--gui" || args[0] == "-g" {
-		fmt.Println("❌ GUI is disabled in this build. Rebuild with: go build -tags gui")
-		os.Exit(1)
-	}
-
-	runCLI(args)
-}
-
 func runCLIMain() {
 	args := os.Args[1:]
 
